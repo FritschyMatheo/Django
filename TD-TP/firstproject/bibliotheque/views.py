c7f0deaaaa27a500c1afd66fb3ecaf77b0579ac7
unknown <matheo.fritschy13@gmail.com>
2024-04-20 09:29:03 +0200

    Ajout fichiers 1

---
+++ TD-TP/firstproject/bibliotheque/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import render
+from .forms import LivreForm
+from . import models
+
+def index(request):
+	return render(request, 'bibliotheque/index.html')
+
+def ajout(request):
+    if request.method == "POST": # arrive en cas de retour sur cette page après une saisie invalide on récupère donc les données. Normalement nous ne devrions pas passer par ce chemin la pour le traitement des données
+        form = LivreForm(request)
+        if form.is_valid(): # validation du formulaire.
+            Livre = form.save() # sauvegarde dans la base
+            return render(request,"bibliotheque/affiche.html",{"Livre" : Livre}) #envoie vers une page d'affichage du Livre créé
+        else:
+            return render(request,"bibliotheque/ajout.html",{"form": form})
+    else :
+        form = LivreForm() # création d'un formulaire vide
+        return render(request,"bibliotheque/ajout.html",{"form" : form})
+
+def traitement(request):
+    lform = LivreForm(request.POST)
+    if lform.is_valid():
+        Livre = lform.save()
+        return render(request,"bibliotheque/affiche.html",{"Livre" : Livre})
+    else:
+        return render(request,"bibliotheque/ajout.html",{"form": lform})
+
+def read(request, id):
+    Livre = models.Livre.objects.get(pk=id) # méthode pour récupérer les données dans la base avec un id donnée
+    return render(request,"bibliotheque/affiche.html",{"Livre": Livre})
