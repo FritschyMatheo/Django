1aa2f9f20c61e482bf34e74e83efaee830305cf3
unknown <matheo.fritschy13@gmail.com>
2024-04-20 11:33:39 +0200

    Avancement bibliotheque

--- TD-TP/firstproject/bibliotheque/urls.py
+++ TD-TP/firstproject/bibliotheque/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('ajout/', views.ajout),
     path('traitement/', views.traitement),
     path('/affiche/<int:id>/',views.read),
+    path('/update/<int:id>/',views.update),
 ]
