f4c01a854757d99ccae7b1e474d52bd05954fb1f
Mathéo <148585740+FritschyMatheo@users.noreply.github.com>
2024-04-26 18:22:32 +0200

    Toujours pas

--- TD-TP/firstproject/bibliotheque/urls.py
+++ TD-TP/firstproject/bibliotheque/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('ajout/', views.ajout),
     path('traitement/', views.traitement),
     path('affiche/<int:id>/',views.read),
-    path('update/<int:id>/',views.traitementupdate),
+    path('update/<int:id>/',views.update),
 ]
