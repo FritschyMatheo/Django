5291f93d91fdea4fbea47019711cfe88a8af49a1
Mathéo <148585740+FritschyMatheo@users.noreply.github.com>
2024-05-12 09:56:10 +0200

    Creation model membre + reorganisation des fichiers pour membres

--- Projet-App/AppMatheo/onepiece/models.py
+++ Projet-App/AppMatheo/onepiece/models.py
@@ -6,7 +6,7 @@ class Equipage(models.Model):
     nom=models.CharField(max_length=100)
     nombateau=models.CharField(max_length=100)
     capitaine=models.CharField(max_length=100)
-    premiereapparition=models.IntegerField(blank=True)
+    premiereapparition=models.IntegerField(null = True, blank=True)
     def __str__(self):
         if self.premiereapparition is not None:
             chaine = f"{self.nom} dirigé par {self.capitaine} est apparu au chapitre {self.premiereapparition} et navigue avec {self.nombateau}."
@@ -16,3 +16,20 @@ class Equipage(models.Model):
     
     def dictionnaire(self):
         return {"nom": self.nom, "nombateau": self.nombateau, "capitaine": self.capitaine, "premiereapparition": self.premiereapparition}
+
+
+class Membre(models.Model):
+    nom=models.CharField(max_length=100)
+    poste=models.CharField(max_length=100)
+    prime=models.IntegerField(null = True, blank=True)
+    fruit=models.CharField(max_length=100, null = True, blank=True)
+    def __str__(self):
+        if self.prime is not None and self.fruit is not None:
+            chaine = f"{self.nom} est {self.poste} de l'équipage. Il a une prime de {self.prime} berrys et possède le pouvoir du fruit : {self.fruit}."
+        elif self.prime is not None:
+            chaine = f"{self.nom} est {self.poste} de l'équipage. Il a une prime de {self.prime} berrys."
+        elif self.fruit is not None:
+            chaine = f"{self.nom} est {self.poste} de l'équipage. Il possède le pouvoir du fruit : {self.fruit}."
+        else:
+            chaine = f"{self.nom} est {self.poste} de l'équipage."
+        return chaine
